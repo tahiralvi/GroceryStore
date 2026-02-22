cd0d7d8832bb2a126dc122801a826a43185d7d2a
Tahir Alvi <engtahiralvi@gmail.com>
2026-02-22 16:33:34 +0500

    update project files

--- GroceryStoreAPI/Controllers/CustomersController.cs
+++ GroceryStoreAPI/Controllers/CustomersController.cs
@@ -1,11 +1,9 @@
-﻿using System;
+﻿using GroceryStoreAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using GroceryStoreAPI.Models;
 
 namespace GroceryStoreAPI.Controllers
 {
--- GroceryStoreAPI/Startup.cs
+++ GroceryStoreAPI/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using GroceryStoreAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,10 +19,9 @@ namespace GroceryStoreAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddDbContext<CustomerContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
-            
+
             services.AddControllers();
         }
 
