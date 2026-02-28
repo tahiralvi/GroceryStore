d5e5f03d4a1167824f09fc4e9dda729732399201
Tahir Alvi <engtahiralvi@gmail.com>
2026-02-28 16:46:20 +0500

    Added Items Service layer, Add items Views
    Added Items Service layer, Add items Views

---
+++ GroceryStoreAPI/Services/Interface/IItemService.cs
@@ -0,0 +1,15 @@
+﻿using GroceryStoreAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GroceryStoreAPI.Services.Interface
+{
+    public interface IItemService
+    {
+        Task<IEnumerable<Item>> GetAllItemsAsync();
+        Task<Item> GetItemByIdAsync(int id);
+        Task AddItemAsync(Item item);
+        Task UpdateItemAsync(Item item);
+        Task DeleteItemAsync(int id);
+    }
+}
---
+++ GroceryStoreAPI/Services/ItemService.cs
@@ -0,0 +1,32 @@
+﻿using GroceryStoreAPI.Models;
+using GroceryStoreAPI.Services.Interface;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GroceryStoreAPI.Services
+{
+    public class ItemService : IItemService
+    {
+        private readonly GroceryContext _context; //
+        public ItemService(GroceryContext context) => _context = context;
+
+        public async Task<IEnumerable<Item>> GetAllItemsAsync() => await _context.Items.ToListAsync();
+        public async Task<Item> GetItemByIdAsync(int id) => await _context.Items.FindAsync(id);
+        public async Task AddItemAsync(Item item)
+        {
+            _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task UpdateItemAsync(Item item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteItemAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+        // Implement Update and Delete similarly...
+    }
+}
