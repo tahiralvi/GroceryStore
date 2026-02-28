3501cbdcbfc351515fdfb60ba3d0be1c797f9e7a
Tahir Alvi <engtahiralvi@gmail.com>
2026-02-28 16:30:59 +0500

    Added items controller. update the context
    Added items controller. update the context

---
+++ GroceryStoreAPI/Controllers/ItemsController.cs
@@ -0,0 +1,27 @@
+﻿using GroceryStoreAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ItemsController : ControllerBase
+{
+    private readonly GroceryContext _context;
+
+    public ItemsController(GroceryContext context) => _context = context;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Item>>> GetItems() => await _context.Items.ToListAsync();
+
+    [HttpPost]
+    public async Task<ActionResult<Item>> PostItem(Item item)
+    {
+        _context.Items.Add(item);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetItems), new { id = item.ItemId }, item);
+    }
+
+    // Add Put and Delete following your Customer pattern
+}
--- GroceryStoreAPI/Models/GroceryContext.cs
+++ GroceryStoreAPI/Models/GroceryContext.cs
@@ -8,5 +8,8 @@ namespace GroceryStoreAPI.Models
         { }
 
         public DbSet<Customer> Customer { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
     }
 }
