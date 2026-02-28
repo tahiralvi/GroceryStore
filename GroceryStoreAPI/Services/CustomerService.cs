0862918aa6f3f8101ece70fcbf7947cd19b40b7c
Tahir Alvi <engtahiralvi@gmail.com>
2026-02-28 16:56:01 +0500

    Add Customer Service components
    Add Customer Service components

---
+++ GroceryStoreAPI/Services/CustomerService.cs
@@ -0,0 +1,24 @@
+﻿using GroceryStoreAPI.Models;
+using GroceryStoreAPI.Services.Interface;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GroceryStoreAPI.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly GroceryContext _context;
+        public CustomerService(GroceryContext context) => _context = context;
+
+        public async Task<IEnumerable<Customer>> GetAllAsync() => await _context.Customer.ToListAsync();
+        public async Task<Customer> GetByIdAsync(int id) => await _context.Customer.FindAsync(id);
+        public async Task CreateAsync(Customer customer) { _context.Add(customer); await _context.SaveChangesAsync(); }
+        public async Task UpdateAsync(Customer customer) { _context.Update(customer); await _context.SaveChangesAsync(); }
+        public async Task DeleteAsync(int id)
+        {
+            var customer = await _context.Customer.FindAsync(id);
+            if (customer != null) { _context.Customer.Remove(customer); await _context.SaveChangesAsync(); }
+        }
+    }
+}
---
+++ GroceryStoreAPI/Services/Interface/ICustomerService.cs
@@ -0,0 +1,15 @@
+﻿using GroceryStoreAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GroceryStoreAPI.Services.Interface
+{
+    public interface ICustomerService
+    {
+        Task<IEnumerable<Customer>> GetAllAsync();
+        Task<Customer> GetByIdAsync(int id);
+        Task CreateAsync(Customer customer);
+        Task UpdateAsync(Customer customer);
+        Task DeleteAsync(int id);
+    }
+}
