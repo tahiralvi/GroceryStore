9388edc2f708fdc282f9114103409d915ad44150
Tahir Alvi <engtahiralvi@gmail.com>
2026-03-01 22:42:26 +0500

    Add Create Item method and view
    Add Create Item method and view

--- GroceryStoreAPI/Controllers/ItemsController.cs
+++ GroceryStoreAPI/Controllers/ItemsController.cs
@@ -1,8 +1,6 @@
 ﻿using GroceryStoreAPI.Models;
 using GroceryStoreAPI.Services.Interface;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 [Route("api/[controller]")]
@@ -17,5 +15,24 @@ public class ItemsController : Controller // Changed from ControllerBase
     {
         var items = await _itemService.GetAllItemsAsync();
         return View(items);
+    }
+    // GET: api/Items/Create
+    [HttpGet("Create")]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: api/Items/Create
+    [HttpPost("Create")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Name,Description,Price,StockQuantity")] Item item)
+    {
+        if (ModelState.IsValid)
+        {
+            await _itemService.AddItemAsync(item);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(item);
     }
 }
